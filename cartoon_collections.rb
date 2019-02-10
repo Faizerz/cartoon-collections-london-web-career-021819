aa80349358c3051decf051568a3ea081ff448cc9
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 17:50:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,11 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
-    puts "#{index}. #{name}"
+    puts "#{index+1}. #{name}"
   end
 end
 
+roll_call_dwarves(dwaves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
